320c1351d31f9359e653c85143899a297e5d9303
Jeongmin Lee <clcl6084@gmail.com>
2025-02-23 19:27:47 +0900

    test(swagger): add swagger

--- apps/swagger/api/index.ts
+++ apps/swagger/api/index.ts
@@ -11,42 +11,42 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+console.log(process.env.VERCEL_URL);
+
+// Swagger setupVERCEL_URL
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Meta Cloud API',
+            version: '1.0.0',
+            description: 'API documentation',
+        },
+        servers: [
+            {
+                url:
+                    process.env.NODE_ENV === 'production'
+                        ? `https://${process.env.VERCEL_URL}`
+                        : 'http://localhost:8080',
+            },
+        ],
+    },
+    apis: ['./src/routes/*.ts', './src/spec/*.spec.yaml'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use('/swagger.json', (req: Request, res: Response) => {
+    res.json(swaggerDocs);
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.redirect('/swagger-ui');
+});
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
-
-// // Swagger setupVERCEL_URL
-// const swaggerOptions = {
-//     swaggerDefinition: {
-//         openapi: '3.0.0',
-//         info: {
-//             title: 'Meta Cloud API',
-//             version: '1.0.0',
-//             description: 'API documentation',
-//         },
-//         servers: [
-//             {
-//                 url:
-//                     process.env.NODE_ENV === 'production'
-//                         ? `https://${process.env.VERCEL_URL}`
-//                         : 'http://localhost:8080',
-//             },
-//         ],
-//     },
-//     apis: ['./src/routes/*.ts', './src/spec/*.spec.yaml'],
-// };
-
-// const swaggerDocs = swaggerJsDoc(swaggerOptions);
-
-// app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// app.use('/swagger.json', (req: Request, res: Response) => {
-//     res.json(swaggerDocs);
-// });
-
-// app.get('/', (req: Request, res: Response) => {
-//     res.redirect('/swagger-ui');
-// });
 
 // app.use('/messages', messageRoutes);
 
