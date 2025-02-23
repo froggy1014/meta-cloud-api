1cd381f90577ea9c33d40e4056831dca35ab6023
Jeongmin Lee <clcl6084@gmail.com>
2025-02-23 20:55:15 +0900

    test(swagger): changes extentions from apis

--- apps/swagger/api/index.ts
+++ apps/swagger/api/index.ts
@@ -36,7 +36,7 @@ const options = {
             },
         ],
     },
-    apis: ['dist/src/routes/*.ts', 'dist/src/spec/*.spec.yaml'],
+    apis: ['dist/src/routes/*.js', 'dist/src/spec/*.spec.yaml'],
 };
 
 const swaggerDocs = swaggerJsDoc(options);
