3404ab117e736e82dc2b59b83f7fe2436759305a
Jeongmin Lee <clcl6084@gmail.com>
2025-03-20 02:10:40 +0900

    chore(swagger): comment out the custtom css

--- apps/swagger/src/routes/apidocs.ts
+++ apps/swagger/src/routes/apidocs.ts
@@ -6,16 +6,16 @@ import openapiDoc from '../schemas/openapi.json';
 export const router = Router();
 
 const customCssUrl = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
-const customCss = `
-    .swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }
-`;
+// const customCss = `
+//     .swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }
+// `;
 
 router.use('/', swaggerUi.serve);
 router.get(
     '/',
     swaggerUi.setup(openapiDoc, {
-        customCss,
         customCssUrl,
+        // customCss,
     }),
 );
 
