b68227d427cbb1feeae91387b82cef3f08de81f7
Jeongmin Lee <clcl6084@gmail.com>
2025-03-20 01:49:24 +0900

    style(swagger): correct broken css

--- apps/swagger/src/routes/apidocs.ts
+++ apps/swagger/src/routes/apidocs.ts
@@ -5,9 +5,18 @@ import openapiDoc from '../schemas/openapi.json';
 
 export const router = Router();
 
-const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+const customCssUrl = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+const customCss = `
+    .swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }
+`;
 
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(openapiDoc, { customCssUrl: CSS_URL }));
+router.get(
+    '/',
+    swaggerUi.setup(openapiDoc, {
+        customCss,
+        customCssUrl,
+    }),
+);
 
 router.use('/openapi.json', express.static(path.join(__dirname, '../schemas/openapi.json')));
