86249a75bd91edf28a37e19f264ff145ccdb83c2
Jeongmin Lee <clcl6084@gmail.com>
2025-04-12 04:08:30 +0900

    chore: delete max-warning 0

--- apps/playground/app/page.tsx
+++ apps/playground/app/page.tsx
@@ -1,7 +1,6 @@
 import Image, { type ImageProps } from 'next/image';
 
 import styles from './page.module.css';
-import { Button } from '@repo/ui/button';
 
 type Props = Omit<ImageProps, 'src'> & {
     srcLight: string;
