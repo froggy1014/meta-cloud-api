2df5a257876329703673d95a318a310ea3490b5d
froggy1014 <clcl6084@gmail.com>
2025-04-17 14:12:52 +0900

    chore(sdk): update version and refine config handling

--- packages/meta-cloud-api/src/utils/importConfig.ts
+++ packages/meta-cloud-api/src/utils/importConfig.ts
@@ -12,11 +12,6 @@ const DEFAULT_MAX_RETRIES_AFTER_WAIT = 30;
 const DEFAULT_REQUEST_TIMEOUT = 20000;
 
 const emptyConfigChecker = (config: WhatsAppConfig | undefined) => {
-    if (!process.env[WabaConfigEnum.PhoneNumberId] && !config?.phoneNumberId) {
-        LOGGER.log(`Environmental variable: WA_PHONE_NUMBER_ID and/or sender phone number id argument is undefined.`);
-        throw new Error('Missing WhatsApp sender phone number Id.');
-    }
-
     if (!process.env[WabaConfigEnum.AccessToken] && !config?.accessToken) {
         LOGGER.log(`Environmental variable: CLOUD_API_ACCESS_TOKEN and/or access token argument is undefined.`);
         throw new Error('Missing WhatsApp access token.');
@@ -31,7 +26,7 @@ export const importConfig = (inputConfig?: WhatsAppConfig) => {
         [WabaConfigEnum.AppId]: process.env.M4D_APP_ID || '',
         [WabaConfigEnum.AppSecret]: process.env.M4D_APP_SECRET || '',
         [WabaConfigEnum.PhoneNumberId]: inputConfig?.phoneNumberId || parseInt(process.env.WA_PHONE_NUMBER_ID || ''),
-        [WabaConfigEnum.BusinessAcctId]: process.env.WA_BUSINESS_ACCOUNT_ID || '',
+        [WabaConfigEnum.BusinessAcctId]: inputConfig?.businessAcctId || process.env.WA_BUSINESS_ACCOUNT_ID || '',
         [WabaConfigEnum.APIVersion]: process.env.CLOUD_API_VERSION || '',
         [WabaConfigEnum.AccessToken]: inputConfig?.accessToken || process.env.CLOUD_API_ACCESS_TOKEN || '',
         [WabaConfigEnum.WebhookEndpoint]: process.env.WEBHOOK_ENDPOINT || '',
