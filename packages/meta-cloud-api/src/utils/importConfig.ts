ee1039cba43849982e8bcca664a97bb3b4505e83
froggy1014 <clcl6084@gmail.com>
2025-05-16 14:15:57 +0900

    refactor(meta-cloud-api): should be able to override the config

--- packages/meta-cloud-api/src/utils/importConfig.ts
+++ packages/meta-cloud-api/src/utils/importConfig.ts
@@ -22,20 +22,25 @@ export const importConfig = (inputConfig?: WhatsAppConfig) => {
     emptyConfigChecker(inputConfig);
 
     const wabaConfig: WabaConfigType = {
-        [WabaConfigEnum.BaseURL]: process.env.WA_BASE_URL || DEFAULT_BASE_URL,
-        [WabaConfigEnum.AppId]: process.env.M4D_APP_ID || '',
-        [WabaConfigEnum.AppSecret]: process.env.M4D_APP_SECRET || '',
+        [WabaConfigEnum.BaseURL]: inputConfig?.baseUrl || process.env.WA_BASE_URL || DEFAULT_BASE_URL,
+        [WabaConfigEnum.AppId]: inputConfig?.appId || process.env.M4D_APP_ID || '',
+        [WabaConfigEnum.AppSecret]: inputConfig?.appSecret || process.env.M4D_APP_SECRET || '',
         [WabaConfigEnum.PhoneNumberId]: inputConfig?.phoneNumberId || Number(process.env.WA_PHONE_NUMBER_ID),
         [WabaConfigEnum.BusinessAcctId]: inputConfig?.businessAcctId || process.env.WA_BUSINESS_ACCOUNT_ID || '',
-        [WabaConfigEnum.APIVersion]: process.env.CLOUD_API_VERSION || '',
+        [WabaConfigEnum.APIVersion]: inputConfig?.apiVersion || process.env.CLOUD_API_VERSION || '',
         [WabaConfigEnum.AccessToken]: inputConfig?.accessToken || process.env.CLOUD_API_ACCESS_TOKEN || '',
-        [WabaConfigEnum.WebhookEndpoint]: process.env.WEBHOOK_ENDPOINT || '',
-        [WabaConfigEnum.WebhookVerificationToken]: process.env.WEBHOOK_VERIFICATION_TOKEN || '',
-        [WabaConfigEnum.ListenerPort]: parseInt(process.env.LISTENER_PORT || '') || DEFAULT_LISTENER_PORT,
+        [WabaConfigEnum.WebhookEndpoint]: inputConfig?.webhookEndpoint || process.env.WEBHOOK_ENDPOINT || '',
+        [WabaConfigEnum.WebhookVerificationToken]:
+            inputConfig?.webhookVerificationToken || process.env.WEBHOOK_VERIFICATION_TOKEN || '',
+        [WabaConfigEnum.ListenerPort]:
+            inputConfig?.listenerPort || parseInt(process.env.LISTENER_PORT || '') || DEFAULT_LISTENER_PORT,
         [WabaConfigEnum.MaxRetriesAfterWait]:
-            parseInt(process.env.MAX_RETRIES_AFTER_WAIT || '') || DEFAULT_MAX_RETRIES_AFTER_WAIT,
-        [WabaConfigEnum.RequestTimeout]: parseInt(process.env.REQUEST_TIMEOUT || '') || DEFAULT_REQUEST_TIMEOUT,
-        [WabaConfigEnum.Debug]: process.env.DEBUG === 'true',
+            inputConfig?.maxRetriesAfterWait ||
+            parseInt(process.env.MAX_RETRIES_AFTER_WAIT || '') ||
+            DEFAULT_MAX_RETRIES_AFTER_WAIT,
+        [WabaConfigEnum.RequestTimeout]:
+            inputConfig?.requestTimeout || parseInt(process.env.REQUEST_TIMEOUT || '') || DEFAULT_REQUEST_TIMEOUT,
+        [WabaConfigEnum.Debug]: inputConfig?.debug || process.env.DEBUG === 'true',
     };
 
     LOGGER.log(`Configuration loaded for App Id ${wabaConfig[WabaConfigEnum.AppId]}`);
