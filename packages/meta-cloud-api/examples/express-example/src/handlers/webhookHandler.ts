63a258564e40c5bad36a1ffbda4c652b0d4187dd
Jeongmin Lee <clcl6084@gmail.com>
2025-05-18 00:14:36 +0900

    docs: update env typo

--- packages/meta-cloud-api/examples/express-example/src/handlers/webhookHandler.ts
+++ packages/meta-cloud-api/examples/express-example/src/handlers/webhookHandler.ts
@@ -9,10 +9,10 @@ import WhatsApp, {
 } from 'meta-cloud-api';
 
 const config = {
-    accessToken: process.env.WHATSAPP_API_KEY || '',
-    phoneNumberId: process.env.WHATSAPP_PHONE_NUMBER_ID ? Number(process.env.WHATSAPP_PHONE_NUMBER_ID) : undefined,
-    businessAcctId: process.env.WHATSAPP_BUSINESS_ACCOUNT_ID || '',
-    webhookVerificationToken: process.env.WEBHOOK_VERIFICATION_TOKEN || 'your_verification_token',
+    accessToken: process.env.CLOUD_API_ACCESS_TOKEN || '',
+    phoneNumberId: process.env.WA_PHONE_NUMBER_ID ? Number(process.env.WA_PHONE_NUMBER_ID) : undefined,
+    businessAcctId: process.env.WA_BUSINESS_ACCOUNT_ID || '',
+    webhookVerificationToken: process.env.WEBHOOK_VERIFICATION_TOKEN || '',
 };
 
 export const webhookHandler = new WebhookHandler(config);
