0288a4da03e16d354f9a5e30540b1ea65bb76610
CockpitConnect <51696821+CockpitConnect@users.noreply.github.com>
2025-05-17 14:32:11 +0200

    Changed message types enum 'Reaction' to the right value (#10)
    Co-authored-by: Dennis van Dalfzen <dennis@saysimple.com>

--- packages/meta-cloud-api/src/types/enums.ts
+++ packages/meta-cloud-api/src/types/enums.ts
@@ -24,7 +24,7 @@ export enum MessageTypesEnum {
     Image = 'image',
     Interactive = 'interactive',
     Location = 'location',
-    Reaction = 'sticker',
+    Reaction = 'reaction',
     Sticker = 'sticker',
     Template = 'template',
     Text = 'text',
