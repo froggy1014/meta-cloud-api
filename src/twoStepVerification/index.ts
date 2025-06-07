ef46581dfaf5d4ee0ac3b14bc0b28866d460815f
Jeongmin Lee <clcl6084@gmail.com>
2025-06-07 23:45:13 +0900

    test(twoStepVerification): add test and refactor twoStepVery structure

---
+++ src/twoStepVerification/index.ts
@@ -0,0 +1,3 @@
+export { default, default as TwoStepVerificationApi } from './TwoStepVerificationApi';
+export * from './types';
+export type { TwoStepVerificationClass, TwoStepVerificationRequest } from './types';
