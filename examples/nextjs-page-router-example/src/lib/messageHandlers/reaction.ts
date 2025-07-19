3281984deca015951ea70700c031d1dba4ab67f0
Jeongmin Lee <clcl6084@gmail.com>
2025-07-20 05:27:34 +0900

    chore: resolve issue

--- examples/nextjs-page-router-example/src/lib/messageHandlers/reaction.ts
+++ examples/nextjs-page-router-example/src/lib/messageHandlers/reaction.ts
@@ -1,13 +1,10 @@
 import type { WhatsApp } from 'meta-cloud-api';
-import { ReactionMessageBuilder } from 'meta-cloud-api/api/messages/builders';
 
 /**
  * Send a reaction to a message
  * Can be used to react with emoji to any message
  */
 export const sendReactionMessage = async (whatsapp: WhatsApp, to: string, messageId: string) => {
-    const reactionMessage = new ReactionMessageBuilder().setMessageId(messageId).setEmoji('👍').build();
-
     await whatsapp.messages.reaction({
         to,
         messageId,
